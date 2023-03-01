690df71f3dfbfaad5ec461df75662da401072640
kijimaD <norimaking777@gmail.com>
2023-03-01 22:49:30 +0900

    fix: run multi analyzers

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+
 	"github.com/kijimaD/my_gsa/gophersample"
 	"github.com/kijimaD/my_gsa/trashcomment"
 	"golang.org/x/tools/go/analysis/unitchecker"
 )
 
 func main() {
-	unitchecker.Main(trashcomment.Analyzer)
-	unitchecker.Main(gophersample.Analyzer)
+	unitchecker.Main(trashcomment.Analyzer, gophersample.Analyzer)
+
+	// test ================
+
+	// trashcomment
+	// a
+
+	// gophersample
+	gopher := "sample"
+	buf := &bytes.Buffer{}
+	fmt.Fprint(buf, gopher)
 }
